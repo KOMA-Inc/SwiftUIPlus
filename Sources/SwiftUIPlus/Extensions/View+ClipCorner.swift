5acfd7be9c95658e6b450b9213c8a8a8a4be1c4b
gloomikon <gloomikon@jatapp.com>
2024-04-25 18:24:16 +0200

    Update clipCorder extension

--- Sources/SwiftUIPlus/Extensions/View+ClipCorner.swift
+++ Sources/SwiftUIPlus/Extensions/View+ClipCorner.swift
@@ -3,11 +3,29 @@ import SwiftUI
 public extension View {
 
     @ViewBuilder
-    func clipCorner(radius: CGFloat?) -> some View {
+    func clipCorner(radius: CGFloat?, corners: UIRectCorner = .allCorners) -> some View {
         if let radius {
-            clipShape(RoundedRectangle(cornerRadius: radius))
+            clipShape(RoundedCorner(cornerRadius: radius, corners: corners))
         } else {
             self
         }
+    }
+}
+
+private struct RoundedCorner: Shape {
+
+    let cornerRadius: CGFloat
+    let corners: UIRectCorner
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(
+                width: cornerRadius,
+                height: cornerRadius
+            )
+        )
+        return Path(path.cgPath)
     }
 }
