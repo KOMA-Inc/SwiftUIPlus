919d654e0a14df31b570669de40ccccddcc4d707
gloomikon <gloomikon@gmail.com>
2024-06-01 12:12:09 +0200

    Update clip corner logic

--- Sources/SwiftUIPlus/Extensions/View+ClipCorner.swift
+++ Sources/SwiftUIPlus/Extensions/View+ClipCorner.swift
@@ -3,34 +3,58 @@ import SwiftUI
 public extension View {
 
     @ViewBuilder
-    func clipCorner(radius: CGFloat?, corners: UIRectCorner = .allCorners) -> some View {
-        if let radius {
-            clipShape(RoundedCorner(cornerRadius: radius, corners: corners))
-        } else {
-            self
-        }
+    func clipCorner(radius: CGFloat = .zero, corners: UIRectCorner = .allCorners) -> some View {
+        clipShape(RoundedRectangle(cornerRadius: radius))
     }
 }
 
-public struct RoundedCorner: Shape {
+@frozen public struct RoundedCorner: Shape {
 
-    let cornerRadius: CGFloat
-    let corners: UIRectCorner
+    private var cornerRadiusWidth: CGFloat
+    private var cornerRadiusHeight: CGFloat
+    private let corners: UIRectCorner
+    var insetAmount: CGFloat = .zero
 
     public init(cornerRadius: CGFloat, corners: UIRectCorner = .allCorners) {
-        self.cornerRadius = cornerRadius
+        self.cornerRadiusWidth = cornerRadius
+        self.cornerRadiusHeight = cornerRadius
         self.corners = corners
     }
 
     public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(
-            roundedRect: rect,
+            roundedRect: CGRect(
+                x: rect.minX + insetAmount,
+                y: rect.minY + insetAmount,
+                width: rect.width - 2 * insetAmount,
+                height: rect.height - 2 * insetAmount
+            ),
             byRoundingCorners: corners,
             cornerRadii: CGSize(
-                width: cornerRadius,
-                height: cornerRadius
+                width: cornerRadiusWidth,
+                height: cornerRadiusWidth
             )
         )
         return Path(path.cgPath)
     }
+
+    public var animatableData: CGSize.AnimatableData {
+        get {
+            CGSize.AnimatableData(cornerRadiusWidth, cornerRadiusHeight)
+        }
+        set {
+            cornerRadiusWidth = newValue.first
+            cornerRadiusHeight = newValue.second
+        }
+    }
+}
+
+extension RoundedCorner: InsettableShape {
+
+    public func inset(by amount: CGFloat) -> some InsettableShape {
+        var shape = self
+        shape.insetAmount = amount
+        return shape
+    }
+
 }
